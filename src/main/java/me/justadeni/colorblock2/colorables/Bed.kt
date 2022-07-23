64157c4451a821447d47cb9646ceed38bb6b0f5c
deninikdo@gmail.com <33rpHbtBEuzxjuT>
2022-07-23 23:45:23 +0200

    worked out regular blocks

--- src/main/java/me/justadeni/colorblock2/colorables/Bed.kt
+++ src/main/java/me/justadeni/colorblock2/colorables/Bed.kt
@@ -4,5 +4,4 @@ import org.bukkit.block.Block
 
 class Bed : Colorable() {
     override val default = "WHITE_BED"
-    override val suffix = "_BED"
 }
--- src/main/java/me/justadeni/colorblock2/colorables/Colorable.kt
+++ src/main/java/me/justadeni/colorblock2/colorables/Colorable.kt
@@ -2,26 +2,46 @@ package me.justadeni.colorblock2.colorables
 
 import me.justadeni.colorblock2.enums.Blocks
 import me.justadeni.colorblock2.enums.Dyes
+import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.Material.getMaterial
 import org.bukkit.block.Block
+import org.bukkit.inventory.ItemStack
 
 abstract class Colorable {
     abstract val default : String
-    abstract val suffix : String
 
-    fun paint(block : Block, dye : String){
-        val dyeSimple = dye.replace("_DYE", "")
-        val blockName = block.type.name
+    fun paint(block : Block, dye : String, dropdye : Boolean){
+        val dyeSimple = dye.replace("_DYE", "") //for example "LIGHT_GRAY"
+        val blockName = block.type.name //uppercase of full block name
 
-
-
-        val newBlock = blockName.replace(Dyes.match(blockName) + "_", dyeSimple + "_" + blockName)
+        val oldDye = Dyes.match(blockName) //dye a block used to have
+        val newBlock = blockName.replace(oldDye, dyeSimple)
         val newBlockMat = getMaterial(newBlock)!!
-        block.setType(newBlockMat)
+        block.type = newBlockMat
+
+        if (dropdye)
+            dropdye(block, oldDye)
     }
 
-    fun unpaint(){
+    fun unpaint(block: Block, dropdye : Boolean){
+        val blockName = block.type.name //uppercase of full block name
+        val oldDye = Dyes.match(blockName) //
 
+        val newBlockMat = getMaterial(default)!!
+        block.type = newBlockMat
+
+        if (dropdye)
+            dropdye(block, oldDye)
+    }
+
+    fun dropdye(block: Block, dye: String){
+        val newDye = dye + "_DYE"
+        val newDyeMat = getMaterial(newDye)!!
+        val loc = block.location
+        val newLoc = Location(block.world, loc.x+0.5, loc.y+0.5, loc.z+0.5)
+
+        block.world.dropItemNaturally(newLoc, ItemStack(newDyeMat))
     }
 }
