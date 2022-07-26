dc021087ddce3dbc99f89b7cfa486ea3bcfb9ca8
deninikdo@gmail.com <33rpHbtBEuzxjuT>
2022-07-26 22:20:03 +0200

    removed pointless console messages

--- src/main/java/me/justadeni/colorblock2/ColorBlock2.kt
+++ src/main/java/me/justadeni/colorblock2/ColorBlock2.kt
@@ -7,12 +7,11 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class ColorBlock2 : JavaPlugin() {
     override fun onEnable() {
-        Bukkit.getLogger().info(colorMsg("&6Plugin Enabled"))
         server.pluginManager.registerEvents(BlockClick, this)
     }
 
     override fun onDisable() {
-        Bukkit.getLogger().info(colorMsg("&3Plugin Disabled"))
+
     }
 
     companion object {
