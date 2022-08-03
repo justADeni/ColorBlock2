d053ac17280cefc5245a8483de9670998b60be6a
justADeni <deninikdo@gmail.com>
2022-08-03 09:40:50 +0200

    minor fix

--- src/main/java/me/justadeni/colorblock2/listeners/BlockClick.kt
+++ src/main/java/me/justadeni/colorblock2/listeners/BlockClick.kt
@@ -18,63 +18,64 @@ import org.bukkit.event.player.PlayerInteractEvent
 object BlockClick : Listener {
 
     @EventHandler
-    suspend fun onBlockClick(e : PlayerInteractEvent) = coroutineScope{
-        launch {
-            if (!(e.player.hasPermission(ColorBlock2.confik.usepermission) || e.player.hasPermission(ColorBlock2.confik.adminpermission)))
-                //return@launch
-                cancel()
-
-            if (e.action != Action.RIGHT_CLICK_BLOCK)
-                cancel()
-
-            val block = e.clickedBlock!!
-            val blockname = block.type.name
-
-            if (Blocks.match(blockname) == "")
-                cancel()
-
-            val player = e.player
-
-            val mainhand = player.inventory.itemInMainHand
-            val offhand = player.inventory.itemInOffHand
-
-            var iscreative: Boolean = player.gameMode == GameMode.CREATIVE
-
-            if (player.isSneaking) {
-                val slot: Boolean = if (mainhand.type.isAir) {
-                    true
-                } else if (offhand.type.isAir) {
-                    false
-                } else {
+    suspend fun onBlockClick(e : PlayerInteractEvent) {
+        coroutineScope {
+            launch {
+                if (!(e.player.hasPermission(ColorBlock2.confik.usepermission) || e.player.hasPermission(ColorBlock2.confik.adminpermission)))
                     cancel()
-                    false
-                }
-                e.isCancelled = true
-                if (iscreative)
-                    if (ColorBlock2.confik.droponcreative)
-                        iscreative = false
-                Uncolor(block, blockname, !iscreative)
-            } else {
-                val slot: Boolean = if ((mainhand.type.name).contains("DYE")) {
-                    true
-                } else if ((offhand.type.name).contains("DYE")) {
-                    false
-                } else {
+
+                if (e.action != Action.RIGHT_CLICK_BLOCK)
                     cancel()
-                    false
-                }
 
-                var dye: String = if (slot) {
-                    mainhand.type.name
+                val block = e.clickedBlock!!
+                val blockname = block.type.name
+
+                if (Blocks.match(blockname) == "")
+                    cancel()
+
+                val player = e.player
+
+                val mainhand = player.inventory.itemInMainHand
+                val offhand = player.inventory.itemInOffHand
+
+                var iscreative: Boolean = player.gameMode == GameMode.CREATIVE
+
+                if (player.isSneaking) {
+                    val slot: Boolean = if (mainhand.type.isAir) {
+                        true
+                    } else if (offhand.type.isAir) {
+                        false
+                    } else {
+                        cancel()
+                        false
+                    }
+                    e.isCancelled = true
+                    if (iscreative)
+                        if (ColorBlock2.confik.droponcreative)
+                            iscreative = false
+                    Uncolor(block, blockname, !iscreative)
                 } else {
-                    offhand.type.name
-                }
-                e.isCancelled = true
-                if (iscreative)
-                    if (ColorBlock2.confik.useoncreative)
-                        iscreative = false
-                Color(block, dye, blockname, player, slot, !iscreative, !iscreative)
+                    val slot: Boolean = if ((mainhand.type.name).contains("DYE")) {
+                        true
+                    } else if ((offhand.type.name).contains("DYE")) {
+                        false
+                    } else {
+                        cancel()
+                        false
+                    }
 
+                    val dye: String = if (slot) {
+                        mainhand.type.name
+                    } else {
+                        offhand.type.name
+                    }
+                    e.isCancelled = true
+                    if (iscreative)
+                        if (ColorBlock2.confik.useoncreative)
+                            iscreative = false
+                    Color(block, dye, blockname, player, slot, !iscreative, !iscreative)
+
+                }
             }
         }
     }
