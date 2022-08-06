3dd017d49dd7304239417cc38da4efb47dd25bdd
deninikdo@gmail.com <33rpHbtBEuzxjuT>
2022-08-06 21:08:19 +0200

    particle system improvement

--- src/main/java/me/justadeni/colorblock2/misc/Particle.kt
+++ src/main/java/me/justadeni/colorblock2/misc/Particle.kt
@@ -1,6 +1,7 @@
 package me.justadeni.colorblock2.misc
 
-import me.justadeni.colorblock2.ColorBlock2
+import com.github.shynixn.mccoroutine.bukkit.ticks
+import kotlinx.coroutines.delay
 import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.block.Block
@@ -16,7 +17,7 @@ object Particle {
 
         val particle: Particle = Particle.valueOf(type.uppercase())
 
-        val loc1 = block.location
+        val loc1 = Location(block.world, block.x+0.5, block.y+0.5, block.z+0.5)
         for (x in -1..1) {
             for (y in -1..1) {
                 for (z in -1..1) {
@@ -27,6 +28,7 @@ object Particle {
                         (block.z + 0.5 + z)
                     )
                     drawLine(loc1, loc2, particle)
+                    delay(2.ticks)
                 }
             }
         }
@@ -35,7 +37,7 @@ object Particle {
     //from https://bukkit.org/threads/making-a-particle-line-from-point-1-to-point-2.465415/
     private suspend fun drawLine(loc1: Location, loc2: Location, particle: Particle) {
         //point2.getWorld()?.let { Validate.isTrue(it.equals(world), "Lines cannot be in different worlds!") }
-        val space = 0.1
+        val space = 0.2
         val distance: Double = loc1.distance(loc2)
         val p1: Vector = loc1.toVector()
         val p2: Vector = loc2.toVector()
